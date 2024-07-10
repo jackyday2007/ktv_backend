af83cbe05d0017a46e6b32a47752667418275c8d
Jeff <xz850412@gmail.com>
2024-07-10 09:59:15 +0800

    update table2

--- KTV/src/main/java/com/ispan/ktv/bean/OrderMenus.java
+++ KTV/src/main/java/com/ispan/ktv/bean/OrderMenus.java
@@ -9,8 +9,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
