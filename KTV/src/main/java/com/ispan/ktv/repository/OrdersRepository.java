b59d4ea79619aed3ce0c0aa39d0583472e047618
Jeff <xz850412@gmail.com>
2024-07-17 11:08:07 +0800

    new

--- KTV/src/main/java/com/ispan/ktv/repository/OrdersRepository.java
+++ KTV/src/main/java/com/ispan/ktv/repository/OrdersRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ispan.ktv.bean.Orders;
-import com.ispan.ktv.dao.OrdersDAO;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long>, JpaSpecificationExecutor<Orders> {
-	
-   long countByOrderDate( Date orderDate);
+
+   long countByOrderDate(Date orderDate);
+
    boolean existsByOrderId(Long orderId);
 
 }
