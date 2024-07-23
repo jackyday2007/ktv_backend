cf4fed4322dc5469c247fdbe78d9c1768cf80ad7
Jacky <jackyday2007@gmail.com>
2024-07-23 12:19:31 +0800

    edit orderMenu

--- KTV/src/main/java/com/ispan/ktv/service/OrderMenuService.java
+++ KTV/src/main/java/com/ispan/ktv/service/OrderMenuService.java
@@ -1,7 +1,6 @@
 package com.ispan.ktv.service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.json.JSONObject;
--- KTV/src/main/java/com/ispan/ktv/service/TimeSlotService.java
+++ KTV/src/main/java/com/ispan/ktv/service/TimeSlotService.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
 
