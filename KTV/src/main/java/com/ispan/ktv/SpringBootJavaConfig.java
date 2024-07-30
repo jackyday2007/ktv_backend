f494400c9548f38d99f85705e9519532b3f05c00
Jacky <jackyday2007@gmail.com>
2024-07-30 15:31:46 +0800

    add

--- KTV/src/main/java/com/ispan/ktv/SpringBootJavaConfig.java
+++ KTV/src/main/java/com/ispan/ktv/SpringBootJavaConfig.java
@@ -1,4 +1,5 @@
 package com.ispan.ktv;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,20 +9,15 @@ import com.ispan.ktv.util.JsonWebTokenInterceptor;
 
 @Configuration
 public class SpringBootJavaConfig implements WebMvcConfigurer {
-	
+
 	@Autowired
 	private JsonWebTokenInterceptor jsonWebTokenInterceptor;
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-<<<<<<< HEAD
-=======
 		System.out.println("registry = " + registry);
->>>>>>> 54243b4dc8bfaa393c1f0828a1b0e33a84e9be75
 		registry.addInterceptor(jsonWebTokenInterceptor)
-		.addPathPatterns("/ktvbackend/**");
+				.addPathPatterns("/ktvbackend/**");
 	}
-	
-	
 
 }
