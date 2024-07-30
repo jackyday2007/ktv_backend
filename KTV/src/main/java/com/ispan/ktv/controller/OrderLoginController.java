04435b95f9870679b0b7b66c937eaccce9824f9f
Jacky <jackyday2007@gmail.com>
2024-07-30 17:42:10 +0800

    add

--- KTV/src/main/java/com/ispan/ktv/controller/OrderLoginController.java
+++ KTV/src/main/java/com/ispan/ktv/controller/OrderLoginController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ispan.ktv.bean.Staff;
@@ -14,6 +15,7 @@ import com.ispan.ktv.util.JsonWebTokenUtility;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
+
 @CrossOrigin
 public class OrderLoginController {
 	
