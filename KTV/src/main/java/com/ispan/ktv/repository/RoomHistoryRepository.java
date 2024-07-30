b11db311ce90f8c1da0d2a71bd3329579635cad9
Jeff <xz850412@gmail.com>
2024-07-30 15:39:10 +0800

    1

--- KTV/src/main/java/com/ispan/ktv/repository/RoomHistoryRepository.java
+++ KTV/src/main/java/com/ispan/ktv/repository/RoomHistoryRepository.java
@@ -4,19 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.ispan.ktv.bean.RoomHistory;
 
 public interface RoomHistoryRepository extends JpaRepository<RoomHistory, Integer> {
-	
-	// 根據時間範圍查詢 RoomHistory
+
+    // 根據時間範圍查詢 RoomHistory
     List<RoomHistory> findByDateBetween(Date startDate, Date endDate);
-//    
-//    @Query("SELECT h FROM RoomHistory h JOIN h.room r " +
-//            "WHERE h.date BETWEEN :startDate AND :endDate")
-//     List<RoomHistory> findRoomHistoryByTimeRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
-//    
-    
+
 }
