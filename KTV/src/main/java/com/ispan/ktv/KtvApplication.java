7f4dfb7a16a199831eb34134122785a604f8a5a4
Jason <ray154236@gmail.com>
2024-07-31 14:13:32 +0800

    123

--- KTV/src/main/java/com/ispan/ktv/KtvApplication.java
+++ KTV/src/main/java/com/ispan/ktv/KtvApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-<<<<<<< HEAD
 @SpringBootApplication
-//(exclude = {SecurityAutoConfiguration.class})
-=======
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
->>>>>>> 44d51a4b390661866ad32173a031a476383d1efa
+// (exclude = {SecurityAutoConfiguration.class})
 public class KtvApplication {
 
 	public static void main(String[] args) {
--- KTV/src/main/java/com/ispan/ktv/SpringBootJavaConfig.java
+++ KTV/src/main/java/com/ispan/ktv/SpringBootJavaConfig.java
@@ -1,49 +1,25 @@
 package com.ispan.ktv;
-<<<<<<< HEAD
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
-//import com.ispan.ktv.util.JsonWebTokenFilter;
+// import com.ispan.ktv.util.JsonWebTokenFilter;
 //
-//@Configuration
-//public class SpringBootJavaConfig implements WebMvcConfigurer {
-//	
-//	@Autowired
-//	private JsonWebTokenFilter jsonWebTokenInterceptor;
-//	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		System.out.println("registry = " + registry);
-//		registry.addInterceptor(jsonWebTokenInterceptor)
-//		.addPathPatterns("/ktvbackend/**");
-//	}
-//	
-//	
+// @Configuration
+// public class SpringBootJavaConfig implements WebMvcConfigurer {
 //
-//}
-=======
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.ispan.ktv.util.JsonWebTokenInterceptor;
-
-@Configuration
-public class SpringBootJavaConfig implements WebMvcConfigurer {
-
-	@Autowired
-	private JsonWebTokenInterceptor jsonWebTokenInterceptor;
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		System.out.println("registry = " + registry);
-		registry.addInterceptor(jsonWebTokenInterceptor)
-				.addPathPatterns("/ktvbackend/**");
-	}
-
-}
->>>>>>> 44d51a4b390661866ad32173a031a476383d1efa
+// @Autowired
+// private JsonWebTokenFilter jsonWebTokenInterceptor;
+//
+// @Override
+// public void addInterceptors(InterceptorRegistry registry) {
+// System.out.println("registry = " + registry);
+// registry.addInterceptor(jsonWebTokenInterceptor)
+// .addPathPatterns("/ktvbackend/**");
+// }
+//
+//
+//
+// }
