0c868e54dd7f9e822eb69b01afdaa822d1d3b895
HHH <aaa26470185@gmail.com>
2024-08-02 20:59:00 +0800

    0802

--- KTV/src/main/java/com/ispan/ktv/bean/Staff.java
+++ KTV/src/main/java/com/ispan/ktv/bean/Staff.java
@@ -22,7 +22,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 // import lombok.ToString;
-
 @Getter
 @Setter
 @NoArgsConstructor
--- KTV/src/main/java/com/ispan/ktv/service/NewsService.java
+++ KTV/src/main/java/com/ispan/ktv/service/NewsService.java
@@ -1,6 +1,7 @@
 package com.ispan.ktv.service;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -13,6 +14,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.expression.ParseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -71,23 +75,19 @@ public class NewsService {
     public void updateNews(Integer newsId, News updatedNews) {
 
         Optional<News> optionalNews = newsRepo.findById(newsId);
-        
 
         if (optionalNews.isPresent()) {
-            JSONObject updateData = new JSONObject();
-            JSON
-            
-            
+            News updateData = optionalNews.get();
+
             // 更新需要修改的屬性
             updateData.setTitle(updatedNews.getTitle());
             updateData.setContent(updatedNews.getContent());
-            updateData.setUrl(updatedNews.getUrl());
             updateData.setStartDate(updatedNews.getStartDate());
             updateData.setEndDate(updatedNews.getEndDate());
             updateData.setStatus(updatedNews.getStatus());
             updateData.setImage(updatedNews.getImage());
             updateData.setActivityStartDate(updatedNews.getActivityStartDate());
-            updateData.setUpdateBy(updatedNews.getUpdateBy().getAccountName());
+            updateData.setUpdateBy(updatedNews.getUpdateBy());
             // 更新 staff 信息
             
 
