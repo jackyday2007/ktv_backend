bc8245f323bede8914eafb3409a36ede8d7d236e
Jason <ray154236@gmail.com>
2024-08-05 17:01:39 +0800

    1

---
+++ KTV/src/main/java/com/ispan/ktv/util/ChangePasswordRequest.java
@@ -0,0 +1,12 @@
+package com.ispan.ktv.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+    private String idNumber;
+    private String oldPassword;
+    private String newPassword;
+}
